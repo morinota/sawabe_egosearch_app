d58f343e96554e3a90954944b99e3cb797f8abfc
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-03-13 23:00:11 +0900

    static生成

---
+++ testapp/static/javascript/sample.js
@@ -0,0 +1,9 @@
+// idが「test」の要素を5秒後に赤くしてるだけ。
+window.onload = function() {
+    var changeColor = function() {
+        var e = document.getElementById('test');
+        e.style.color = 'red';
+        console.log("書き換えテスト")
+    }
+    setTimeout(changeColor, 5000);
+}
